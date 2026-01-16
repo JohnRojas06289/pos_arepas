5c54ad798649299bef5ae85eed81e43653024b78
JohnRojas06289 <79581520+JohnRojas06289@users.noreply.github.com>
2026-01-16 13:43:03 -0500

    Update debug route to show filesystem info

--- routes/debug.php
+++ routes/debug.php
@@ -2,5 +2,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/debug-php', function () {
-    return phpinfo();
+    $debug = [
+        'filesystem_default' => config('filesystems.default'),
+        'disk_config' => config('filesystems.disks.cloudinary'),
+        'env_url' => env('CLOUDINARY_URL'),
+        'test_url' => \Illuminate\Support\Facades\Storage::url('test.jpg'),
+        'php_upload_max' => ini_get('upload_max_filesize'),
+        'php_post_max' => ini_get('post_max_size'),
+    ];
+    return response()->json($debug);
 });
