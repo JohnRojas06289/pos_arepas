024f1be9eb4a8b8a5c78c135f37ea7bf0d3913a6
JohnRojas06289 <79581520+JohnRojas06289@users.noreply.github.com>
2026-01-16 14:44:02 -0500

    Align navbar icons to the right

--- resources/views/layouts/include/navigation-header.blade.php
+++ resources/views/layouts/include/navigation-header.blade.php
@@ -110,7 +110,7 @@ if (!$empresa) {
         <i class="fas fa-utensils me-2"></i>{{ $empresa->nombre ?? 'POS Arepas' }}
     </a>
     <!-- Sidebar Toggle-->
-    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!">
+    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0 ms-auto" id="sidebarToggle" href="#!">
         <i class="fas fa-bars"></i>
     </button>
 
